5c03f03692d865256fd8a6cb610698f308fdaa07
bhaskkkarrr <bhaskarchauhan748@gmail.com>
2026-05-27 11:36:39 +0530

    trial changes for route

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,10 @@ import StepsCard from "../components/StepsCard";
 import FeaturesCards from "../components/FeaturesCards";
 import Footer from "../components/Footer";
 import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full min-h-screen max-w-4xl mx-auto flex flex-col">
       <div className="mx-3 my-4 space-y-8">
@@ -20,6 +23,7 @@ const Home = () => {
             whileHover={{ scale: 1.03 }}
             whileTap={{ scale: 0.97 }}
             className="bg-velvet-orchid text-mauve-50 px-5 py-2 rounded-full md:text-2xl text-lg font-semibold"
+            onClick={() => navigate("/auth")}
           >
             Start Interview
           </motion.button>
